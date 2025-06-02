a77cf4d5d0e2c4b2a1fea172842a825b02d51b75
kravchenski <max5045955@gmail.com>
2025-06-02 10:24:23 +0200

    feat: create artist task

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), UserModule, TrackModule, AlbumModule],
+  }), UserModule, TrackModule, AlbumModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/artist/dto/update-artist.dto.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateArtistDto } from './create-artist.dto';
+
+export class UpdateArtistDto extends PartialType(CreateArtistDto) {}
