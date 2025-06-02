c1f9ad3ab2b45f1389ec263ad170d9286f27aec5
kravchenski <max5045955@gmail.com>
2025-06-02 10:03:33 +0200

    feat: create track task

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), UserModule],
+  }), UserModule, TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/track/dto/update-track.dto.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTrackDto } from './create-track.dto';
+
+export class UpdateTrackDto extends PartialType(CreateTrackDto) {}
